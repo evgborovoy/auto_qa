829f5a4b675aa8e5ad84a9791282d0e8632f37d3
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-03-27 05:09:57 +0400

    change driver options

--- conftest.py
+++ conftest.py
@@ -10,10 +10,10 @@ def driver():
 
     # options for run in container
     options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
     yield driver
     driver.quit()
